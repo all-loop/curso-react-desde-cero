29988e4767023586fbe0b24e15be5412048a75b5
Gonzalo FU <GFU-code@protonmail.ch>
2021-12-07 11:52:48 -0300

    section 04 - Retorno de elementos/etiquetas dentro de un Componente de React

--- section04  - primeros pasos/counter-app/src/PrimeraApp.js
+++ section04  - primeros pasos/counter-app/src/PrimeraApp.js
@@ -10,11 +10,31 @@
  *  1. Las basadas en clases
  *  2. Las basadas en funciones
  */
+// import React, { Fragment } from "react";
 import React from "react";
 
 // Functional components
 const PrimeraApp = () => {
-  return <h1>Hola, Mundo!</h1>;
+  /**
+   * Devolver más de una etiqueta
+   * ............................
+   *
+   * Un componente solo puede devolver una etiqueta, la cuál puede o no anidar más
+   * etiquetas dentro de ella.
+   *
+   * Para anidar etiquetas podriamos:
+   * 1. Crear una etiqueta General, por ejemplo <div>contenido</div>. Esta forma crea una
+   *  etiqueta div dentro del dom.
+   * 2. Usando el Componente Fragment ofrecido por React, <Fragment>contenido</Fragment>. Esta forma
+   *  sólo incluye su contenido en el dom.
+   * 3. Usando la síntaxis <>conenido</>. Al igual que fragment, sólo incluye su contenido en el dom.
+   */
+  return (
+    <>
+      <h1>Hola, Mundo!</h1>
+      <p>Mi primera aplicación</p>
+    </>
+  );
 };
 
 export default PrimeraApp;
